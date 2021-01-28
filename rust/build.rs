8a5a5447b0dde5a5675f147c6d95e5c13d18f0c8
Gabriel Wu <wuzihua@pku.edu.cn>
2021-01-28 11:36:46 +0800

    feat: use tonic for code generation

--- rust/build.rs
+++ rust/build.rs
@@ -1,20 +1,21 @@
-fn main() {
-    protoc_rust::Codegen::new()
-        .out_dir("src/puppet")
-        .inputs(&[
-            "src/protos/puppet.proto",
-            "src/protos/puppet/base.proto",
-            "src/protos/puppet/contact.proto",
-            "src/protos/puppet/event.proto",
-            "src/protos/puppet/file_box.proto",
-            "src/protos/puppet/friendship.proto",
-            "src/protos/puppet/message.proto",
-            "src/protos/puppet/room.proto",
-            "src/protos/puppet/room_invitation.proto",
-            "src/protos/puppet/room_member.proto",
-            "src/protos/puppet/tag.proto",
-        ])
-        .includes(&["src/protos"])
-        .run()
-        .expect("protoc");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(false)
+        .compile(
+            &[
+                "src/protos/puppet.proto",
+                "src/protos/puppet/base.proto",
+                "src/protos/puppet/contact.proto",
+                "src/protos/puppet/event.proto",
+                "src/protos/puppet/file_box.proto",
+                "src/protos/puppet/friendship.proto",
+                "src/protos/puppet/message.proto",
+                "src/protos/puppet/room.proto",
+                "src/protos/puppet/room_invitation.proto",
+                "src/protos/puppet/room_member.proto",
+                "src/protos/puppet/tag.proto",
+            ],
+            &["src/protos"],
+        )?;
+    Ok(())
 }
